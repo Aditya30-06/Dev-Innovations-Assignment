b3d3e67c8af1eb6787d4f5c865d8fd17107cf8ff
Aditya30-06 <aditya123adf@gmail.com>
2025-09-07 18:52:50 +0530

    backend url

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URI || 'http://localhost:5000/api';
+const API_BASE_URL = "https://dev-innovations-assignment.onrender.com";
 
 // Create axios instance
 const api = axios.create({
