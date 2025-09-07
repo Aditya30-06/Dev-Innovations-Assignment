1b7b6fa5745b46761b0337c2d5ac2d8381fb438b
Aditya30-06 <aditya123adf@gmail.com>
2025-09-07 21:04:04 +0530

    final one

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://dev-innovations-assignment.onrender.com";
+const API_BASE_URL = "https://dev-innovations-assignment.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
